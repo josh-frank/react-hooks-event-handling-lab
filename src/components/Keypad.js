d2b3668629cd5bb9ac323ee67750aa1d5e03dccd
Josh Frank <eurosewusa@gmail.com>
2021-02-22 15:56:29 -0500

    commit tree b5a3bb9438bd24e106c8b49fd85afef2963919f6

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function enteringPassword() {
+    console.log( "Entering password..." );
+}
+
+function Keypad() {
+    return (
+        <input type="password" onChange={ enteringPassword }></input>
+    );
+}
+
+export default Keypad;
